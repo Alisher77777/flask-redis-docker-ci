880c1442a3a832e9fb016cbc8000a535a1ed1099
Alisher77777 <166746238+Alisher77777@users.noreply.github.com>
2025-05-08 23:40:17 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask
 import redis
+import logging
 
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.INFO)
+
 r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    count = r.incr('hits')
-    return f'Hello! This page has been viewed {count} times.'
+    r.set('message', 'Hello from Redis!')
+    redis_message = r.get('message').decode('utf-8')
+
+    app.logger.info('Main page accessed')
+
+    return f'Flask is working! Redis says: {redis_message}'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host="0.0.0.0", debug=True)
